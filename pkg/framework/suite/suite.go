15392e9a77a42a2830c6f510d0f84e1bc6a8d3f2
Sinyaev Anton Yuryevich <asinyaev@ozon.ru>
2022-06-03 16:52:02 +0300

    [066] fix package naming for internal suites

--- pkg/framework/suite/suite.go
+++ pkg/framework/suite/suite.go
@@ -22,15 +22,38 @@ func (s *Suite) SetRunner(runner runner.TestRunner) {
 
 func (s *Suite) RunSuite(t provider.T, suite runner.InternalSuite) {
 	t.SkipOnPrint()
-	RunSuite(t.RealT(), suite)
+	runner.NewSuiteRunner(t.RealT(), getPackage(2), t.Name(), suite).RunTests()
+}
+
+func (s *Suite) RunNamedSuite(t provider.T, suiteName string, suite runner.InternalSuite) {
+	t.SkipOnPrint()
+	runner.NewSuiteRunner(t.RealT(), getPackage(2), suiteName, suite).RunTests()
 }
 
 func RunSuite(t provider.TestingT, suite runner.InternalSuite) map[string]bool {
-	return runner.NewSuiteRunner(t, getPackage(2), t.Name(), suite).RunTests()
+	return runner.NewSuiteRunner(t, getPackage(2), cleanName(t.Name()), suite).RunTests()
 }
 
 func RunNamedSuite(t provider.TestingT, suiteName string, suite runner.InternalSuite) map[string]bool {
 	return runner.NewSuiteRunner(t, getPackage(2), suiteName, suite).RunTests()
+}
+
+func cleanName(fullName string) string {
+	nameParts := strings.Split(fullName, "/")
+	var removeIdxs []int
+	for idx, namePart := range nameParts {
+		if strings.HasSuffix(namePart, "_Tests") {
+			removeIdxs = append(removeIdxs, idx)
+		}
+	}
+	for _, idx := range removeIdxs {
+		nameParts = remove(nameParts, idx)
+	}
+	return strings.Join(nameParts, "/")
+}
+
+func remove(slice []string, s int) []string {
+	return append(slice[:s], slice[s+1:]...)
 }
 
 func getPackage(depth int) string {
