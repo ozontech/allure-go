2b8e09072d04103819b359e08f72680c37642bb9
koodeex <belomorovich@gmail.com>
2022-06-07 01:36:27 +0300

    [v067] fix relative path support for test plan feature

--- pkg/framework/core/allure_manager/testplan/testplan.go
+++ pkg/framework/core/allure_manager/testplan/testplan.go
@@ -11,9 +11,8 @@ import (
 )
 
 var (
-	once = sync.Once{}
-
-	testPlan *TestPlan
+	once     = sync.Once{}
+	testPlan = initTestPlan()
 )
 
 // Path to testplan.json
@@ -29,22 +28,6 @@ type TestPlan struct {
 	Tests   []*TestCase `json:"tests"`
 }
 
-func GetTestPlan() *TestPlan {
-	var (
-		err error
-	)
-
-	testPlanOnce := func() {
-		testPlan, err = newTestPlan()
-		if err != nil {
-			fmt.Printf("%s\n", err.Error())
-		}
-	}
-
-	once.Do(testPlanOnce)
-	return testPlan
-}
-
 func newTestPlan() (*TestPlan, error) {
 	filePath := os.Getenv(testPlanPath)
 	if filePath == "" {
@@ -54,19 +37,25 @@ func newTestPlan() (*TestPlan, error) {
 		return nil, fmt.Errorf("%s environment variable has a wrong format. Please, set path to .json file. Current path:%s", testPlanPath, filePath)
 	}
 
-	testPlanRaw, err := ioutil.ReadFile(filepath.Clean(filePath))
-	if err != nil {
-		return nil, err
+	testPlanRaw, readFileErr := findTestPlan(filePath)
+	if readFileErr != nil {
+		return nil, readFileErr
 	}
 
 	plan := &TestPlan{}
-	err = json.Unmarshal(testPlanRaw, plan)
+	err := json.Unmarshal(testPlanRaw, plan)
 	if err != nil {
 		return nil, err
 	}
-	return testPlan, nil
+
+	if plan != nil && len(plan.Tests) == 0 {
+		return nil, fmt.Errorf("no any tests found in %s", filePath)
+	}
+	return plan, nil
 }
 
+// IsSelected returns true if selector matches with testplan selector
+// TODO: ID parsing from TestOps
 func (p *TestPlan) IsSelected(id, selector string) bool {
 	for _, t := range p.Tests {
 		if t.Selector == selector {
@@ -74,4 +63,63 @@ func (p *TestPlan) IsSelected(id, selector string) bool {
 		}
 	}
 	return false
+}
+
+func initTestPlan() *TestPlan {
+	var (
+		err   error
+		tPlan *TestPlan
+	)
+
+	testPlanOnce := func() {
+		tPlan, err = newTestPlan()
+		if err != nil {
+			fmt.Printf("%s\n", err.Error())
+		}
+	}
+	once.Do(testPlanOnce)
+	return tPlan
+}
+
+// GetTestPlan ...
+func GetTestPlan() *TestPlan {
+	return testPlan
+}
+
+func findTestPlan(path string) (testPlanRaw []byte, readFileErr error) {
+	testPlanRaw, readFileErr = ioutil.ReadFile(filepath.Clean(path))
+	if readFileErr == nil && testPlanRaw != nil {
+		return testPlanRaw, nil
+	}
+	dir, getWdErr := os.Getwd()
+	if getWdErr != nil {
+		return nil, getWdErr
+	}
+
+	pathParts := strings.Split(dir, string(os.PathSeparator))
+
+	// windows absolute path workaround
+	// issue describing: https://github.com/golang/go/issues/26953#issuecomment-412447719
+	if strings.HasSuffix(pathParts[0], ":") {
+		pathParts[0] = pathParts[0] + "/"
+	}
+
+	// os.Getwd() returns current test folder.
+	// trying to walk up the absolute path to find testplan.json
+	tmpPathParts := pathParts
+	for _, _ = range pathParts {
+		basicPath := filepath.Join(tmpPathParts...)
+		absolutePath := filepath.Join(basicPath, filepath.Clean(path))
+
+		testPlanRaw, readFileErr = ioutil.ReadFile(absolutePath)
+		if readFileErr != nil {
+			// stop looking if project root found
+			_, gErr := ioutil.ReadFile(filepath.Join(basicPath, "go.mod"))
+			if gErr == nil {
+				return
+			}
+			tmpPathParts = tmpPathParts[:len(tmpPathParts)-1]
+		}
+	}
+	return
 }
