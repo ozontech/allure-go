de732bb1cfb9e7cc0cae90708ee2e9d9cacecb8f
Sinyaev Anton Yuryevich <asinyaev@ozon.ru>
2022-08-22 16:44:06 +0300

    [ISSUE-30] add example for test setup

---
+++ examples/suite_demo/setup_test.go
@@ -0,0 +1,45 @@
+package suite_demo
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ozontech/allure-go/pkg/framework/provider"
+	"github.com/ozontech/allure-go/pkg/framework/suite"
+)
+
+type SetupSuite struct {
+	suite.Suite
+}
+
+func (s *SetupSuite) TestMyTest(t provider.T) {
+	var (
+		v1  string
+		v2  int
+		ctx context.Context
+	)
+	t.WithTestSetup(func(t provider.T) {
+		t.WithNewStep("init v1", func(sCtx provider.StepCtx) {
+			v1 = "string"
+			sCtx.WithNewParameters("v1", v1)
+		})
+		t.WithNewStep("init v2", func(sCtx provider.StepCtx) {
+			v2 = 123
+			sCtx.WithNewParameters("v2", v2)
+		})
+		t.WithNewStep("init ctx", func(sCtx provider.StepCtx) {
+			ctx = context.Background()
+			sCtx.WithNewParameters("ctx", ctx)
+		})
+	})
+	defer t.WithTestTeardown(func(t provider.T) {
+		t.WithNewStep("Close ctx", func(sCtx provider.StepCtx) {
+			ctx.Done()
+			sCtx.WithNewParameters("ctx", ctx)
+		})
+	})
+}
+
+func TestRunner(t *testing.T) {
+	suite.RunSuite(t, new(SetupSuite))
+}
