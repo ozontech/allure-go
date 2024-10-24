cd73e160fc314986a4922ebdb20a5aef5bee979b
Sergey Makarov <siller174@gmail.com>
2024-10-24 17:37:31 +0200

    fix data race in text ctx [checktoberfest][hacktoberfest]

--- pkg/framework/core/allure_manager/ctx/test_ctx.go
+++ pkg/framework/core/allure_manager/ctx/test_ctx.go
@@ -4,18 +4,24 @@ import (
 	"github.com/ozontech/allure-go/pkg/allure"
 	"github.com/ozontech/allure-go/pkg/framework/core/constants"
 	"github.com/ozontech/allure-go/pkg/framework/provider"
+	"sync"
 )
 
 type testCtx struct {
-	name   string
+	name string
+
+	m      sync.RWMutex
 	result *allure.Result
 }
 
 func NewTestCtx(result *allure.Result) provider.ExecutionContext {
-	return &testCtx{result: result, name: constants.TestContextName}
+	return &testCtx{result: result, name: constants.TestContextName, m: sync.RWMutex{}}
 }
 
 func (ctx *testCtx) AddStep(newStep *allure.Step) {
+	ctx.m.Lock()
+	defer ctx.m.Unlock()
+
 	ctx.result.Steps = append(ctx.result.Steps, newStep)
 }
 
@@ -24,5 +30,8 @@ func (ctx *testCtx) GetName() string {
 }
 
 func (ctx *testCtx) AddAttachments(attachments ...*allure.Attachment) {
+	ctx.m.Lock()
+	defer ctx.m.Unlock()
+
 	ctx.result.Attachments = append(ctx.result.Attachments, attachments...)
 }
